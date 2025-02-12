d5a3b08465318f332f9f17db371c4231b0066256
milesbryan-prog <miles.bryan@ufl.edu>
2025-02-12 09:56:39 -0500

    Update riemann.py
    all errors fixed, unsure if value returned is correct

--- riemann.py
+++ riemann.py
@@ -3,13 +3,16 @@ import numpy as np
 def left_endpoint(x_vals: np.ndarray, func: np.ufunc)->float:
     x_values_shifted = np.array([0])
     size = len(x_vals)
-    np.append(x_vals, 6)
-    x_values_shifted = np.stack(x_values_shifted, x_vals)
+    #np.append(x_vals, 6)
+    x_values_shifted = np.concatenate((x_values_shifted, x_vals))
     x_vals_function = func(x_vals[:size - 1])
-    x_values_shifted = x_values_shifted[0: size - 1]
-    x_vals.reshape(1,1)
-    x_values_shifted.reshape(1,1)
-    multiply_by = np.subtract(x_vals - x_values_shifted)
+    x_values_shifted = x_values_shifted[0: size]
+    x_vals.reshape(1,size)
+    x_values_shifted.reshape(1,size)
+
+    multiply_by = np.subtract(x_vals, x_values_shifted)
+    size_final = len(multiply_by)
+    multiply_by =  multiply_by[:size_final - 1]
     final = multiply_by * x_vals_function
     final = sum(final)
     return final
