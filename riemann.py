452d01739b88abb2e88ae2267a1a4dcf4459118f
milesbryan-prog <miles.bryan@ufl.edu>
2025-02-12 20:50:51 -0500

    Update riemann.py
    Added trapizodal function appears to work properly :) 👍

--- riemann.py
+++ riemann.py
@@ -11,8 +11,18 @@ def left_endpoint(x_vals: np.ndarray, func: np.ufunc)->float:
     x_values_shifted.reshape(1,size)
 
     multiply_by = np.subtract(x_vals, x_values_shifted)
+    print(multiply_by)
+    print(x_vals_function)
     size_final = len(multiply_by)
     multiply_by =  multiply_by[:size_final - 1]
     final = multiply_by * x_vals_function
     final = sum(final)
     return final
+
+
+def trapezoid(x_vals: np.ndarray, func: np.ufunc)->float:
+    size = len(x_vals)
+    x_vals_function = func(x_vals[:size - 1])
+    n = (x_vals[size-1] - x_vals[0]) / size
+    final_trap = n * sum(x_vals_function)
+    return final_trap
